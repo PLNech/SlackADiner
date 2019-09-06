8e2fa6c6bbb7e50c6c33f4570910fb041c0294a2
Haroen Viaene <hello@haroen.me>
2019-09-06 17:35:28 +0200

    future-proof for "update"

--- slackbot.py
+++ slackbot.py
@@ -7,8 +7,8 @@ client = slack.WebClient(
 )
 
 
-def format_text(fr, en, number):
-    return """
+def make_message(fr, en, number):
+    text = """
 Hi everyone :sir:
 
 Today you can eat *{en}* (_{fr}_ :cow:)
@@ -18,27 +18,42 @@ There is {number} left, hurry!
         fr=fr, en=en, number=number
     )
 
+    attachments = [
+        {
+            "fallback": "Grab the food at https://55-amsterdam.sohappy.work/index.cfm?e=zr&id=1968",
+            "actions": [
+                {
+                    "type": "button",
+                    "text": "Grab the food 😋",
+                    "url": "https://55-amsterdam.sohappy.work/index.cfm?e=zr&id=1968",
+                }
+            ],
+        }
+    ]
+
+    return text, attachments
+
 
 def send():
+    text, attachments = make_message(*get_meal())
     response = client.chat_postMessage(
         channel="#test",
-        text=format_text(*get_meal()),
-        attachments=[
-            {
-                "fallback": "Grab the food at https://55-amsterdam.sohappy.work/index.cfm?e=zr&id=1968",
-                "actions": [
-                    {
-                        "type": "button",
-                        "text": "Grab the food 😋",
-                        "url": "https://55-amsterdam.sohappy.work/index.cfm?e=zr&id=1968",
-                    }
-                ],
-            }
-        ],
+        # channel="#office-paris-lunch",
+        text=text,
+        attachments=attachments,
     )
     print("message sent!")
     return response
 
+def update(channel_id, ts):
+    text, attachments = make_message(*get_meal())
+    response = client.chat_update(
+        channel=channel_id,
+        ts=ts,
+        text=text,
+        attachments=attachments,
+    )
 
 if __name__ == "__main__":
-    send()
+    response = send()
+    print(response) # this contains channel & ts, used for updating
