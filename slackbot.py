621039a35c04851a62b10bc6d1cb1c7818f3fb1d
Paul-Louis NECH <pln@algolia.com>
2019-09-06 14:51:59 +0200

    feat(slackbot): stuff

--- slackbot.py
+++ slackbot.py
@@ -3,35 +3,40 @@ import slack
 from diner import get_meal
 
 client = slack.WebClient(
-  token=os.environ['SLACK_API_TOKEN']
+    token=os.environ["SLACK_API_TOKEN"]
 )
 
+
 def format_text(fr, en):
-  return """
+    return """
 Hi everyone :sir:
 
 Today you can eat *{en}* (_{fr}_ :cow:)
-""".format(fr=fr, en=en)
+""".format(
+        fr=fr, en=en
+    )
+
 
 def send():
-  response = client.chat_postMessage(
-    channel='#test',
-    text=format_text(*get_meal()),
-    attachments=[
-      {
-        "fallback": "Grab the food at https://55-amsterdam.sohappy.work/index.cfm?e=zr&id=1968",
-        "actions": [
-          {
-            "type": "button",
-            "text": "Grab the food 😋",
-            "url": "https://55-amsterdam.sohappy.work/index.cfm?e=zr&id=1968"
-          },
-        ]
-      }
-    ]
-  )
-  print('message sent!')
-  return response
+    response = client.chat_postMessage(
+        channel="#test",
+        text=format_text(*get_meal()),
+        attachments=[
+            {
+                "fallback": "Grab the food at https://55-amsterdam.sohappy.work/index.cfm?e=zr&id=1968",
+                "actions": [
+                    {
+                        "type": "button",
+                        "text": "Grab the food 😋",
+                        "url": "https://55-amsterdam.sohappy.work/index.cfm?e=zr&id=1968",
+                    }
+                ],
+            }
+        ],
+    )
+    print("message sent!")
+    return response
+
 
 if __name__ == "__main__":
-  send()
+    send()
