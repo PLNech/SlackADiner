e2e7b90b02e1e55bce7748bb9d8aba6e0ba9a07f
Paul-Louis NECH <pln@algolia.com>
2019-09-15 14:57:28 +0200

    test: unittest slackbot

---
+++ test.py
@@ -0,0 +1,43 @@
+import unittest
+from datetime import date
+
+from slackbot import SlackBot
+
+mock_meal = ("Aubergines sautées", "sautéed eggplants", 10)
+
+
+class SlackBotTestCase(unittest.TestCase):
+    """ Tests for slackbot runner. """
+
+    def setUp(self):
+        self.bot = SlackBot()
+
+    def test_is_canteen_day(self):
+        saturday = date(2000, 1, 1)
+        sunday = date(2000, 1, 2)
+        monday = date(2000, 1, 3)
+        tuesday = date(2000, 1, 4)
+        wednesday = date(2000, 1, 5)
+        thursday = date(2000, 1, 6)
+        friday = date(2000, 1, 7)
+        self.assertFalse(self.bot.is_canteen_day(saturday), "No canteen on saturdays")
+        self.assertFalse(self.bot.is_canteen_day(sunday), "No canteen on sundays")
+        self.assertTrue(self.bot.is_canteen_day(monday), "Canteen on mondays")
+        self.assertTrue(self.bot.is_canteen_day(tuesday), "Canteen on tuesdays")
+        self.assertTrue(self.bot.is_canteen_day(wednesday), "Canteen on wednesdays")
+        self.assertTrue(self.bot.is_canteen_day(thursday), "Canteen on thursdays")
+        self.assertTrue(self.bot.is_canteen_day(friday), "Canteen on fridays")
+
+    def test_make_message(self):
+        text, attachments = self.bot.make_message([])
+        self.assertIsNone(attachments, "Empty meals should have no attachments")
+
+        text, attachments = self.bot.make_message([mock_meal])
+        self.assertIsNotNone(attachments, "Meals should have some attachments")
+
+    def test_format_meal(self):
+        (fr, en, q) = mock_meal
+        text = self.bot.format_meal(fr, en, q)
+        self.assertTrue(fr in text, "The text should contain the french meal")
+        self.assertTrue(en in text, "The text should contain the english meal")
+        self.assertTrue(str(q) in text, "The text should contain the quantity")
